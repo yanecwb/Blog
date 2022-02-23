e471bf84d0a08c4b0acde0a79eb4b54c2c2ab112
noah <londonnleon@gmail.com>
2022-02-23 15:50:06 +0800

    footer_init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div id="app">
-    <Header msg="Welcome to Your Vue.js App"/>
-    <Banner/>
-    <Container/>
+    <Header msg="Welcome to Your Vue.js App" />
+    <Banner />
+    <Container />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/header/index.vue'
-import Banner from './views/banner/index.vue'
-import Container from './views/container/index.vue'
+import Header from "./components/header/index.vue";
+import Footer from "./components/footer/index.vue";
+import Banner from "./views/banner/index.vue";
+import Container from "./views/container/index.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
+    Footer,
     Banner,
-    Container
-  }
-}
+    Container,
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
-    font-family: 'Noto Sans JP';
+  font-family: "Noto Sans JP";
 }
 </style>
