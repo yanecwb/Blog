fda6cd65149a54562b74953ae556197fb8cefbcc
yaner <574164478@qq.com>
2022-06-20 16:33:21 +0800

    代码块样式、复制按钮

--- src/views/article_detail/index.vue
+++ src/views/article_detail/index.vue
@@ -3,13 +3,13 @@
   <div class="w-full  h-aotu" style="background-color:#f4f8fb">
    <iframe frameborder="0" scrolling="no" src="http://47.107.243.60:5003/colokBanner.html" class="w-full" height="500px"></iframe>
     <div
-      class="w-full md:w-3/5 md:pt-14 p-3 pt-1 mx-auto border-4 border-light-blue-500 border-opacity-100 bg-white opacity-75 shadow-2xl);"
+      class="w-full md:w-3/5 md:mt-14 p-3 pt-1 mx-auto border-4 border-light-blue-500 border-opacity-100 bg-white shadow-2xl);"
     >
       <div ><Icon type="left" class="text-white"/></div>
-      <div class="text-base md:text-2xl lg:text-3xl font-bold md:px-32 mt-3 md:mt-0">
+      <div class="text-base md:text-2xl lg:text-3xl font-bold md:px-1 mt-3 md:mt-0">
         {{ article.article_title }}
       </div>
-      <div class="w-full flex justify-between items-center my-3 md:px-10">
+      <div class="w-full flex justify-between items-center my-3 px-1 md:px-1">
         <div class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12">
           <img
             :src="article.uper.avatarUrl"
@@ -36,7 +36,7 @@
           修改
         </button>
       </div>
-      <div v-html="article.content" class="h px-3 md:px-24"></div>
+      <div v-html="article.content" class=" px-3 md:px-18"></div>
     </div>
   </div>
 </template>
@@ -64,6 +64,16 @@ export default {
 </script>
 
 <style lang="less">
+pre{
+  background-color: #f6f8fa;
+  display: flex;
+  justify-content: space-between;
+}
+pre::after{
+  content: '复制';
+  color: rgb(190, 179, 179);
+  cursor: pointer;
+}
 .h {
   img {
     width: 100%;
