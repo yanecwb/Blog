e6c2f68572d3b48e2947269056d23b0c9cf01953
yaner <574164478@qq.com>
2022-07-11 16:14:37 +0800

    添加备案信息

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <footer class="w-full h-20 md:h-40 opacity-90  bg-122 flex justify-center items-center flex-wrap"  :class="$store.state.isfixed ? 'fixed bottom-0':''">
-      <p class="text-white">Flechazo的博客💗</p>
-      <p class="text-white">记得留下足迹哦</p>
+    <footer class="w-full h-20 md:h-40 opacity-90  bg-122 flex justify-center items-center flex-col flex-wrap"  :class="$store.state.isfixed ? 'fixed bottom-0':''">
+        <p class="text-white w-auto">Flechazo的博客💗记得留下足迹哦</p>
+        <p class=" text-#99 text-xs">© 2022 Flechazo  <a href="https://beian.miit.gov.cn" class=" text-#99 text-xs" target="_blank">赣ICP备2022006183号-1</a></p>
       <!-- <p class="text-white">Jenkins自动部署</p> -->
     </footer>
   </div>
